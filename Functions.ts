89d4cc698975aec25744995f8fd99dbdcfc80a69
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 15:33:49 +0530

    types of function

---
+++ Functions.ts
@@ -0,0 +1,33 @@
+//? Functions
+
+//! Function types
+
+//* simple function
+// function greet(name) {
+//   return `Hello, ${name}`;
+// }
+
+//* variabl function
+// const greet = function (name) {
+//   return `Hello, ${name}`;
+// };
+
+//* arrow function
+// const greet = (name) => `Hello, ${name}`;
+
+//* Anonymous Functions
+// setTimeout(function() {
+//   console.log('This is an anonymous function!');
+// }, 1000);
+
+//* Immediately Invoked Function Expression (IIFE)
+// (function () {
+//   console.log("This is an IIFE!");
+// })();
+
+//* Async Functions
+// async function fetchData() {
+//   const response = await fetch('https://api.example.com/data');
+//   const data = await response.json();
+//   return data;
+// }
