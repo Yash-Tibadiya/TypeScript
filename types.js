ddb1efd8f70b093b8ce0b67d68de647a67361f35
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 12:08:44 +0530

    Add detailed examples of TypeScript types including enums, any, unknown, void, and never

--- types.js
+++ types.js
@@ -17,3 +17,51 @@ let y = x;
 y.pop();
 console.log(x); // [1, 2]
 console.log(y); // [1, 2]
+/*************************************************************************/
+// number: let a = 1.23
+// string: let b = "hello"
+// boolean: let c = true
+// null:
+let d;
+// undefined:
+let e;
+// array:
+let array = [1, 2, 3];
+// tuple:
+let tuple = [1, "hello"];
+// enum:
+var UserRole;
+(function (UserRole) {
+    UserRole["ADMIN"] = "admin";
+    UserRole["USER"] = "user";
+    UserRole["GUEST"] = "guest";
+})(UserRole || (UserRole = {}));
+UserRole.ADMIN;
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 200] = "SUCCESS";
+    StatusCode[StatusCode["NOT_FOUND"] = 404] = "NOT_FOUND";
+    StatusCode[StatusCode["SERVER_ERROR"] = 500] = "SERVER_ERROR";
+})(StatusCode || (StatusCode = {}));
+StatusCode.SUCCESS;
+// any:
+let anyValue;
+anyValue = 123;
+anyValue = "hello";
+// unknown:
+let unknownValue;
+unknownValue = 123;
+unknownValue = "hello";
+if (typeof unknownValue === "string") {
+    unknownValue.toUpperCase();
+}
+// void: if function returns nothing then use void
+function abcd() {
+    console.log("hey");
+}
+// never: if function never returns then use never
+function abc() {
+    while (true) { }
+}
+abc();
+// console.log(abc()); // error
--- types.ts
+++ types.ts
@@ -21,3 +21,60 @@ y.pop();
 
 console.log(x); // [1, 2]
 console.log(y); // [1, 2]
+
+/*************************************************************************/
+// number: let a = 1.23
+// string: let b = "hello"
+// boolean: let c = true
+
+// null:
+let d: null;
+// undefined:
+let e: undefined;
+
+// array:
+let array = [1, 2, 3];
+
+// tuple:
+let tuple: [number, string] = [1, "hello"];
+
+// enum:
+enum UserRole {
+  ADMIN = "admin",
+  USER = "user",
+  GUEST = "guest",
+}
+UserRole.ADMIN;
+
+enum StatusCode {
+  SUCCESS = 200,
+  NOT_FOUND = 404,
+  SERVER_ERROR = 500,
+}
+StatusCode.SUCCESS;
+
+// any:
+let anyValue: any;
+anyValue = 123;
+anyValue = "hello";
+
+// unknown:
+let unknownValue: unknown;
+unknownValue = 123;
+unknownValue = "hello";
+
+if (typeof unknownValue === "string") {
+  unknownValue.toUpperCase();
+}
+
+// void: if function returns nothing then use void
+function abcd(): void {
+  console.log("hey");
+}
+
+// never: if function never returns then use never
+function abc(): never {
+  while (true) {}
+}
+abc();
+// console.log(abc()); // error
