5b026086bb49f6a448b2dc05ab5ba5c3858dfc08
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 17:32:54 +0530

    Type Assertion, Type Casting, Non-null Assertion

---
+++ TypeAssertion.js
@@ -0,0 +1,18 @@
+"use strict";
+//? Type Assertion, Type Casting, Non-null Assertion
+/*************************************************************************/
+//! Type Assertion
+let n = 12;
+//* n is number so it shows error but it gives all string methods
+// console.log((n as string).length);
+console.log(n);
+/*************************************************************************/
+//! Type Casting
+let n1 = Number("12");
+console.log(n1, typeof n1);
+/*************************************************************************/
+//! Non-null Assertion
+let n2;
+n2 = "hello";
+let n3 = n2.toUpperCase();
+console.log(n3);
