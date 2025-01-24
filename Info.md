8641404853cd439f3107860d1322f612a5b35203
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 11:21:32 +0530

    Basic type & Other Types in Typescript

---
+++ Info.md
@@ -0,0 +1,9 @@
+# Typescript commands
+
+To install : `npm install -g typescript@latest`
+
+To initialize : `npx tsc --init`
+
+To compile : `tsc app.ts`
+
+To watch : `tsc --watch app.ts`
---
+++ app.ts
@@ -0,0 +1,8 @@
+//? Basic Types :
+// primitive types : (number, string, boolean, null, undefined, symbol, bigint)
+
+//? Other :
+// Array, Tuple, Enum, any, void, never, unknown
+// Object {}, Union |, Intersection &,
+// Literal Types, Template Literal Types,
+// Type Assertions [(someValue as string) & (<string>someValue)]
