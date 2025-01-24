f9e071cd1f652f314b4b374742b10012259917f7
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 15:51:28 +0530

    Function types { string, void, function }

--- ClassesAndObjects.js
+++ ClassesAndObjects.js
@@ -1,34 +1,19 @@
+"use strict";
 //? Classes & Objects
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 /*************************************************************************/
 //! Class definition
-var Device = /** @class */ (function () {
-    function Device() {
+class Device {
+    constructor() {
         this.name = "LG";
         this.price = 12000;
         this.category = "digital";
     }
-    return Device;
-}());
-var tv = new Device();
-var monitor = new Device();
+}
+let tv = new Device();
+let monitor = new Device();
 // Constructors
-var HumanMaker = /** @class */ (function () {
-    function HumanMaker(name, isHandsome, photo) {
+class HumanMaker {
+    constructor(name, isHandsome, photo) {
         this.name = name;
         this.isHandsome = isHandsome;
         this.photo = photo;
@@ -37,37 +22,30 @@ var HumanMaker = /** @class */ (function () {
             this.photo = "blank.jpg";
         }
     }
-    return HumanMaker;
-}());
-var human = new HumanMaker("Rohit", true, "");
+}
+let human = new HumanMaker("Rohit", true, "");
 /*************************************************************************/
 //! public, private, protected
 // public changeable any where
 // private changeable only inside class
 // protected changeable only inside class and inheritance
-var BottleMaker = /** @class */ (function () {
-    function BottleMaker(name, price, color) {
+class BottleMaker {
+    constructor(name, price, color) {
         this.name = name;
         this.price = price;
         this.color = color;
     }
-    BottleMaker.prototype.changingPrice = function () {
+    changingPrice() {
         this.price = 1000;
-    };
-    return BottleMaker;
-}());
-var ColorBottleMaker = /** @class */ (function (_super) {
-    __extends(ColorBottleMaker, _super);
-    function ColorBottleMaker() {
-        return _super !== null && _super.apply(this, arguments) || this;
     }
-    ColorBottleMaker.prototype.changeColor = function () {
+}
+class ColorBottleMaker extends BottleMaker {
+    changeColor() {
         this.color = "blue";
-    };
-    return ColorBottleMaker;
-}(BottleMaker));
-var bottle = new BottleMaker("viva", 1200, "red");
-var newBottle = new ColorBottleMaker("viva", 1200, "red");
+    }
+}
+let bottle = new BottleMaker("viva", 1200, "red");
+let newBottle = new ColorBottleMaker("viva", 1200, "red");
 // public
 bottle.name = "Milton"; // changed
 // private
@@ -80,29 +58,27 @@ console.log(bottle);
 console.log(newBottle);
 /*************************************************************************/
 //! Readonly property
-var Employee = /** @class */ (function () {
-    function Employee(name) {
+class Employee {
+    constructor(name) {
         this.name = name;
     }
-    Employee.prototype.changeName = function () {
+    changeName() {
         // this.name = "Rohit"; // changed but {TS show error}
-    };
-    return Employee;
-}());
-var emp = new Employee("Yash");
+    }
+}
+let emp = new Employee("Yash");
 emp.changeName();
 console.log(emp);
 /*************************************************************************/
 //! Optional property
-var Person = /** @class */ (function () {
-    function Person(name, age) {
+class Person {
+    constructor(name, age) {
         this.name = name;
         this.age = age;
     }
-    return Person;
-}());
-var person1 = new Person("yash", 20);
-var person2 = new Person("Rohit");
+}
+let person1 = new Person("yash", 20);
+let person2 = new Person("Rohit");
 console.log(person1);
 console.log(person2);
 /*************************************************************************/
@@ -115,67 +91,50 @@ console.log(person2);
 //     this.price = price;
 //   }
 // }
-var Bottle = /** @class */ (function () {
-    function Bottle(name, price) {
+class Bottle {
+    constructor(name, price) {
         this.name = name;
         this.price = price;
     }
-    return Bottle;
-}());
+}
 /*************************************************************************/
 //! Getter & Setter
-var newUser = /** @class */ (function () {
-    function newUser(_name) {
+class newUser {
+    constructor(_name) {
         this._name = _name;
     }
-    Object.defineProperty(newUser.prototype, "name", {
-        get: function () {
-            return this._name;
-        },
-        set: function (value) {
-            this._name = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return newUser;
-}());
-var user = new newUser("Yash");
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+}
+let user = new newUser("Yash");
 user.name = "Rohit";
 console.log(user);
 /*************************************************************************/
 //! Static members
-var ToDoApp = /** @class */ (function () {
-    function ToDoApp() {
-    }
-    ToDoApp.getRandomNumber = function () {
+class ToDoApp {
+    static getRandomNumber() {
         return Math.floor(Math.random() * 10 + 1);
-    };
-    ToDoApp.version = "1.0.0";
-    return ToDoApp;
-}());
+    }
+}
+ToDoApp.version = "1.0.0";
 console.log(ToDoApp.version);
 console.log(ToDoApp.getRandomNumber());
 /*************************************************************************/
 //! Abstract classes and methods
-var Animal = /** @class */ (function () {
-    function Animal() {
-    }
-    Animal.prototype.move = function () {
+class Animal {
+    move() {
         console.log("Moving...");
-    };
-    return Animal;
-}());
-var Dog = /** @class */ (function (_super) {
-    __extends(Dog, _super);
-    function Dog() {
-        return _super !== null && _super.apply(this, arguments) || this;
     }
-    Dog.prototype.sound = function () {
+}
+class Dog extends Animal {
+    sound() {
         console.log("Bark");
-    };
-    return Dog;
-}(Animal));
-var dog = new Dog();
+    }
+}
+const dog = new Dog();
 dog.sound();
 dog.move();
--- Functions.js
+++ Functions.js
@@ -1,51 +1,41 @@
+"use strict";
 //? Functions
-
 /*************************************************************************/
 //! Types of Function
-
 //* simple function
 // function greet(name) {
 //   return `Hello, ${name}`;
 // }
-
 //* variabl function
 // const greet = function (name) {
 //   return `Hello, ${name}`;
 // };
-
 //* arrow function
 // const greet = (name) => `Hello, ${name}`;
-
 //* Anonymous Functions
 // setTimeout(function() {
 //   console.log('This is an anonymous function!');
 // }, 1000);
-
 //* Immediately Invoked Function Expression (IIFE)
 // (function () {
 //   console.log("This is an IIFE!");
 // })();
-
 //* Async Functions
 // async function fetchData() {
 //   const response = await fetch('https://api.example.com/data');
 //   const data = await response.json();
 //   return data;
 // }
-
 /*************************************************************************/
 //! Function types
-
 // function test(): string {
 //   return "test";
 // }
-
 // function test(): void {}
-
-function test(name: string, callback: (value1: string, value2: string) => void) {
-  callback("xyz", name);
+function test(name, callback) {
+    callback("xyz", name);
 }
-test("Yash", (value1: string, value2: string) => {
-  console.log(value1);
-  console.log(value2);
+test("Yash", (value1, value2) => {
+    console.log(value1);
+    console.log(value2);
 });
