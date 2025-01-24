f851e6248cdc312e8cd51162a71a540c00a89bd8
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 11:38:42 +0530

    What is primitive & reference types

--- types.ts
+++ types.ts
@@ -1,8 +1,23 @@
-"use strict";
 //? Basic Types :
 // primitive types : (number, string, boolean, null, undefined, symbol, bigint)
+
 //? Other :
 // Array, Tuple, Enum, any, void, never, unknown
 // Object {}, Union |, Intersection &,
 // Literal Types, Template Literal Types,
 // Type Assertions [(someValue as string) & (<string>someValue)]
+
+// primitive types
+let a = 10;
+let b = a;
+console.log(a); // 10
+console.log(b + 2); // 12
+
+// reference types
+let x = [1, 2, 3];
+let y = x;
+
+y.pop();
+
+console.log(x); // [1, 2]
+console.log(y); // [1, 2]
