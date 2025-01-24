3e79117e3eb1df11d8dd624833a3c3280657b0c7
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 12:14:41 +0530

    Diff: Type Interface and Type annotation

---
+++ Interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+// Type Interface : typescript automatically "infers"{decide} the type of the variable
+let a = 12;
+// Type annotation
+let b;
+b = 12;
+let c;
+c = 12;
+c = "hello";
+c = true;
---
+++ Interfaces.ts
@@ -0,0 +1,12 @@
+// Type Interface : typescript automatically "infers"{decide} the type of the variable
+let a = 12;
+
+// Type annotation
+let b: number;
+b = 12;
+
+let c: number | string | boolean;
+c = 12;
+c = "hello";
+c = true;
+
