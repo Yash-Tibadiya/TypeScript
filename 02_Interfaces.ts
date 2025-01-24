c4d9b0c242ff5f3b2eef4fbc96b122741a8e3a33
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-24 18:08:50 +0530

    Refactor file name

--- 02_Interfaces.ts
+++ 02_Interfaces.ts
@@ -1,10 +1,11 @@
-"use strict";
 // Type Interface : typescript automatically "infers"{decide} the type of the variable
 let a1 = 12;
+
 // Type annotation
-let b1;
+let b1: number;
 b1 = 12;
-let c;
+
+let c: number | string | boolean;
 c = 12;
 c = "hello";
 c = true;
--- 07_ImModules.ts
+++ 07_ImModules.ts
@@ -1,11 +1,11 @@
 // Import functions
-import { addPayment, getDetails } from "./ExModules";
+import { addPayment, getDetails } from "./07_ExModules";
 
 addPayment(12000);
 getDetails();
 
 // Import classes
-import ToDoApp from "./ExModules";
+import ToDoApp from "./07_ExModules";
 
 let app = new ToDoApp("1.0.0");
 console.log(app.version);
